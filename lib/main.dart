338aabc98eb00f4a6f6ad3ed792b444f2e6f41ea
joesevv <joseph.severino1@gmail.com>
2026-09-10 16:05:09 -0500

    made text bigger

--- lib/main.dart
+++ lib/main.dart
@@ -104,10 +104,13 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('Joseph have pushed the button this many times:'),
+            const Text('Joseph has pushed the button this many times:'),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: TextStyle(
+                fontSize: 124.0, // Adjust this number to make it bigger
+                fontWeight: FontWeight.bold, // Optional: makes it bold too
+              ),
             ),
           ],
         ),
